84c4b49b5e650855d37fe6ca8d34c384b03df65e
djtriple9 <117500842+djtriple9@users.noreply.github.com>
2022-11-06 14:41:26 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,82 +1,49 @@
-/* 
- * This example sends data from an Adafruit-compatible BMP280 
- * temperature and pressure sensor (located in the engine room)
- * to a Signal K Server. Comments here in the program file are
- * minimal - for a full explanation, see
- * https://github.com/SignalK/SensESP/pages/tutorials/bmp280
- * 
- * Here's the sensor: https://www.adafruit.com/product/2651
- *
- * NOTE: Use this as a template for almost any other Arduino-compatible
- * physical sensor.
-*/
+; PlatformIO Project Configuration File
+;
+;   Build options: build flags, source filter
+;   Upload options: custom upload port, speed and extra flags
+;   Library options: dependencies, extra library storages
+;   Advanced options: extra scripting
+;
+; Please visit documentation for the other options and examples
+; https://docs.platformio.org/page/projectconf.html
 
-// Boilerplate #includes:
-#include "sensesp_app_builder.h"
-#include "sensesp/signalk/signalk_output.h"
+; This is the platformio.ini file to be used with the BMP280 
+; Tutorial / example.
 
-// Sensor-specific #includes:
-#include <Adafruit_BMP280.h>
-#include <Wire.h>
-// For RepeatSensor:
-#include "sensesp/sensors/sensor.h"
+[platformio]
+; set default_envs to whichever board(s) you use. Build/Run/etc processes those envs
+default_envs = 
+;   esp32dev
 
-using namespace sensesp;
+[env]
+; Global data for all [env:***]
+framework = arduino
+lib_ldf_mode = deep
+monitor_speed = 115200
+lib_deps =
+  SignalK/SensESP @ ^2.6.0
+  adafruit/DHT sensor library @ ^1.4.4
+  adafruit/Adafruit Unified Sensor @ ^1.1.6
 
-// SensESP builds upon the ReactESP framework. Every ReactESP application
-// must instantiate the "app" object.
-reactesp::ReactESP app;
+[espressif32_base]
+;this section has config items common to all ESP32 boards
+platform = espressif32
+build_unflags =
+  -Werror=reorder
+board_build.partitions = min_spiffs.csv
+monitor_filters = esp32_exception_decoder
 
-// (Specific to the BMP280, and I2C. Replace this with similar code to create an instance
-// of whatever sensor you may be using in your project.)
-// Create an instance of the sensor using its I2C interface.
-Adafruit_BMP280 bmp280;
+[env:esp32dev]
+extends = espressif32_base
+board = esp32dev
+build_flags =
+  -D LED_BUILTIN=2
+  ; Uncomment the following to disable debug output altogether
+  ;-D DEBUG_DISABLED
+  ; Uncomment the following to enable the remote debug telnet interface on port 23
+  ;-D REMOTE_DEBUG
 
-// (Replace this with whatever function you need to read whatever value you want
-// to read from any other sensor you're using in your project.)
-// Define the function that will be called every time we want
-// an updated temperature value from the sensor. The sensor reads degrees
-// Celsius, but all temps in Signal K are in Kelvin, so add 273.15.
-float read_temp_callback() { return (bmp280.readTemperature() + 273.15); }
-
-// The setup function performs one-time application initialization
-void setup() {
-
-// Some initialization boilerplate when in debug mode
-#ifndef SERIAL_DEBUG_DISABLED
-  SetupSerialDebug(115200);
-#endif
-
-  // Create the global SensESPApp() object
-  SensESPAppBuilder builder;
-  sensesp_app = builder.get_app();
-
-  // (Do whatever is required to "start" your project's sensor here)
-  // Initialize the BMP280 using the default address
-  bmp280.begin();
-
-  // Read the sensor every 2 seconds
-  unsigned int read_interval = 2000;
-
-  // Create a RepeatSensor with float output that reads the temperature
-  // using the function defined above.
-  auto* engine_room_temp =
-      new RepeatSensor<float>(read_interval, read_temp_callback);
-
-  // Set the Signal K Path for the output
-  const char* sk_path = "propulsion.engineRoom.temperature";
-
-  // Send the temperature to the Signal K server as a Float
-  engine_room_temp->connect_to(new SKOutputFloat(sk_path));
-
-  // Start the SensESP application running
-  sensesp_app->start();
-
-}
-
-// loop simply calls `app.tick()` which will then execute all reactions as needed
-void loop() {
-  
-  app.tick();
-  
-}
+;; Uncomment and change these if PlatformIO can't auto-detect the ports
+;upload_port = /dev/tty.SLAB_USBtoUART
+;monitor_port = /dev/tty.SLAB_USBtoUART
